6021052cec779cbe922011f08095da6b14e56baa
Furkan Ar <far.farnt@gmail.com>
2023-07-04 20:33:34 +0300

    logging added

--- Notification/src/main/java/com/demo/Blog/Notification/converter/MailConverter.java
+++ Notification/src/main/java/com/demo/Blog/Notification/converter/MailConverter.java
@@ -2,6 +2,8 @@ package com.demo.Blog.Notification.converter;
 
 import com.demo.Blog.Notification.model.Mail;
 import com.demo.Blog.Notification.request.MailRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -10,13 +12,17 @@ import java.util.UUID;
 @Component
 public class MailConverter {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     public Mail convert(MailRequest mailRequest){
+        logger.info("convert to Mail method started");
         Mail mail = new Mail();
         mail.setEmail(mailRequest.getEmail());
         mail.setMessage(mailRequest.getMessage());
         mail.setFullName(mailRequest.getFullName());
         mail.setUserName(mailRequest.getUserName());
         mail.setCreateDate(LocalDateTime.now());
+        logger.info("convert to Mail method successfully worked");
         return mail;
 
     }
--- Notification/src/main/java/com/demo/Blog/Notification/listener/NotificationListener.java
+++ Notification/src/main/java/com/demo/Blog/Notification/listener/NotificationListener.java
@@ -4,6 +4,8 @@ import com.demo.Blog.Notification.repository.MailRepository;
 import com.demo.Blog.Notification.converter.MailConverter;
 import com.demo.Blog.Notification.model.Mail;
 import com.demo.Blog.Notification.request.MailRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,7 @@ public class NotificationListener {
     private final MailRepository mailRepository;
     private final MailConverter mailConverter;
 
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     public NotificationListener(MailRepository mailRepository, MailConverter mailConverter) {
         this.mailRepository = mailRepository;
@@ -20,9 +23,11 @@ public class NotificationListener {
     }
 
     @RabbitListener(queues = "blog.confirmation.notification")
-    public void notifcationListener(MailRequest mailRequest){
+    public void notificationListener(MailRequest mailRequest){
+        logger.info("Notification listener invoked - Consuming Message with EmailRequest Email: {}", mailRequest.getEmail());
         Mail mail = mailConverter.convert(mailRequest);
-        mailRepository.save(mail);
-
+        Mail savedMail = mailRepository.save(mail);
+        logger.info("Mail created: {} ", savedMail.getId());
+        logger.info("notificationListener method successfully worked");
     }
 }
