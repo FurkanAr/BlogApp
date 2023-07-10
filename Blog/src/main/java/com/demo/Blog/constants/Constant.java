7dd37c8a3fb11ba76650272e87b004287882f812
Furkan Ar <far.farnt@gmail.com>
2023-07-10 14:21:08 +0300

    constant created

---
+++ Blog/src/main/java/com/demo/Blog/constants/Constant.java
@@ -0,0 +1,11 @@
+package com.demo.Blog.constants;
+
+public class Constant {
+
+    public static class Authentication{
+        public static final String LOGIN_MESSAGE = "User successfully login!!";
+        public static final String REGISTRATION_MAIL_MESSAGE = "Welcome to the blog app!!";
+        public static final String REGISTRATION_MESSAGE = "User successfully registered!!";
+
+    }
+}
